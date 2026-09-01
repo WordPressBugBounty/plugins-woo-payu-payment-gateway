8403b678d7cd975171e867a4b5ccbc8f94404b0a
Sean Murphy <sean@iamseanmurphy.com>
2026-09-01 10:02:23 +0000

    2.10.2

--- woo-payu-payment-gateway/vendor/composer/installed.php
+++ woo-payu-payment-gateway/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'payu/payment_gateway',
-        'pretty_version' => '2.10.1',
-        'version' => '2.10.1.0',
+        'pretty_version' => '2.10.2',
+        'version' => '2.10.2.0',
         'reference' => null,
         'type' => 'woocommerce-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,17 +11,17 @@
     ),
     'versions' => array(
         'openpayu/openpayu' => array(
-            'pretty_version' => '2.3.6',
-            'version' => '2.3.6.0',
-            'reference' => '3d5a609147777e2ba64d72957492457cdd67d239',
+            'pretty_version' => '2.4.2',
+            'version' => '2.4.2.0',
+            'reference' => 'f6fb871d373b5b2f451608206771d2da7a2cd67d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../openpayu/openpayu',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'payu/payment_gateway' => array(
-            'pretty_version' => '2.10.1',
-            'version' => '2.10.1.0',
+            'pretty_version' => '2.10.2',
+            'version' => '2.10.2.0',
             'reference' => null,
             'type' => 'woocommerce-plugin',
             'install_path' => __DIR__ . '/../../',
